3f4483bc68c4183d2bd415a4edf0218659a89e4a
nalden141 <noor15511551@gmail.com>
2024-05-16 22:44:45 +0300

    Initial commit

---
+++ roles/web/files/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const enterButton = document.getElementById("enterButton");
+
+    enterButton.addEventListener("click", function() {
+        alert("Welcome to our website!");
+    });
+});
